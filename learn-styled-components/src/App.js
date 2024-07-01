c01f37a0f9c99c2d2cb2ca938d674622e884082e
su <parangwavetraveller@gmail.com>
2024-07-01 20:43:17 +0900

    Styled Components - Super Recap

--- learn-styled-components/src/App.js
+++ learn-styled-components/src/App.js
@@ -1,55 +1,69 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+
+  h1 {
+    color: tomato;
+
+    &:hover {
+      color: cornflowerblue;
+    }
+  }
+
+  ${Title}:hover {
+    color: blue;
+  }
 `;
 
-const rotation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  } 50% {
-    border-radius: 50%;
+const anime = keyframes`
+  from {
+    color: tomato;
   }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0%;
+  to {
+    color: lime;
   }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-  transition: all 0.2s ease-in-out;
 `;
 
 const Box = styled.div`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotation} 1s linear infinite;
-  /* target styled-component */
-  ${Emoji}:hover {
-    font-size: 100px;
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+const Btn = styled.button`
+  background-color: orange;
+  padding: 50px;
+  animation: ${anime} 0.7s infinite;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  height: 50px;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>✅</Emoji>
+      <Box bgColor="tomato" />
+      <Circle bgColor="teal" />
+      <Btn as="a" href="#">
+        Login
+      </Btn>
+      <Input />
+      <h1>hello world</h1>
+      <Title>hi world</Title>
     </Wrapper>
   );
 }
