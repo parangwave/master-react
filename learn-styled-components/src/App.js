66f0a6d01708acc377d32770209ab6d2d7466be5
su <parangwavetraveller@gmail.com>
2024-07-01 19:39:29 +0900

    Styled Components - Adapting props and Extending components

--- learn-styled-components/src/App.js
+++ learn-styled-components/src/App.js
@@ -4,31 +4,23 @@ const Parent = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
+// adapting props
+const Box = styled.div`
   background-color: teal;
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.h1`
-  color: white;
+// extending comp using styled()
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Parent>
-      {/* <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div> */}
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      {/* <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div> */}
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Parent>
   );
 }
