104dbec5d027335882f78372e182ec648def2e57
su <parangwavetraveller@gmail.com>
2024-07-03 22:48:44 +0900

    React Router v6 - extras w/ useSearchParams, useLoaderdata, loader, Form, etc

--- react-router-v6/src/screens/Home.tsx
+++ react-router-v6/src/screens/Home.tsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  // console.log(readSearchParams); // URLSearchParams (js class that has utility methods)
+  // console.log(readSearchParams.has("geo")); // true
+  // console.log(readSearchParams.get("geo")); // 123
+
+  // modify url programmatically
+  setTimeout(() => {
+    setSearchParams({
+      day: "today",
+      tomorrow: "123",
+    });
+  }, 3000);
+  // http://localhost:3000/?day=today&tomorrow=123
+
   return (
     <div>
       <h1>Users</h1>
