b773b7a93a8a2671b315af73e0fc0a4555b4c367
su <parangwavetraveller@gmail.com>
2024-07-03 18:43:32 +0900

    React Router v6 - useNavigate

--- react-router-v6/src/components/Header.tsx
+++ react-router-v6/src/components/Header.tsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    // 2. useNavigate(): locate or change the location of user
+    // ex. if user login and needs to redirect
+    // or user got access-denied from a page
+    // or remove user from pages programmatically
+    // = w/ no waiting for click
+    navigate("/about");
+  };
+
   return (
     <h1>
       <ul>
         <li>
+          {/* Ways to locate to other pages */}
+          {/* 1. Link: needs to click */}
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </h1>
