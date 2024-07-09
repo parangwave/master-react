ab383fad5553b89234c27415929ea5618746052a
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-09 14:37:53 +0900

    To Do Setup

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,16 @@
+// global style
+import GlobalStyle from "./GlobalStyle";
+
+// Components
+import ToDoList from "./ToDoList";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
---
+++ todo-list/src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3542",
+  textColor: "#f5f6fa",
+  accentColor: "#2ed573",
+  boxShadow: "rgba(0, 0, 0, 0.27) 0px 1px 4px",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#2f3542",
+  accentColor: "#1B9CFC",
+  boxShadow: "rgba(0, 0, 0, 0.27) 0px 1px 4px",
+};
