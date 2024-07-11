9ce98dba1f847574473b808542d1e825456c9c3a
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-11 13:21:11 +0900

    Animations - Variants pt.2 - control animations on children
    많은 animation을 하나로 합칠 수 있음. 부모 컴포넌트의 variants에서 delayChildren, staggerChildren를 통해 자식 component의 animation도 제어 할 수 있음.

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, spring, Variants } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -15,31 +15,65 @@ const Box = styled(motion.div)`
   background-color: white;
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  place-self: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 // variants = stage for animations
 // 컴포넌트가 가질 수 있는 미리 정의된 시각적 state
 // 1. help you clean code (mv setting to seperated js obj)
-const myVariants = {
+// 2. combine many animations
+const boxVariants: Variants = {
   start: {
-    scale: 0,
+    opacity: 0,
+    scale: 0.5,
   },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
     transition: {
       type: "spring",
-      delay: 1,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.3,
     },
+  },
+};
+
+const cicleVariants: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 export default function App() {
   return (
     <Wrapper>
-      {/* initial="prop1", animate="prop2" */}
-      {/* prop1, prop2 must be in variants obj */}
-      <Box variants={myVariants} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* <Circle initial="start" animate="end" /> */}
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+      </Box>
     </Wrapper>
   );
 }
