27c251db3347f48822fbe8062b126f1885b1552d
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-12 23:42:30 +0900

    Animations - Gestures pt.1
    whileHover, whileDrag, whileTap(whileClick)

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -17,17 +17,8 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  place-self: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -36,44 +27,22 @@ const Circle = styled(motion.div)`
 // 1. help you clean code (mv setting to seperated js obj)
 // 2. combine many animations
 const boxVariants: Variants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.3,
-    },
-  },
-};
-
-const cicleVariants: Variants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  // numeric value when animate color
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 5 } },
 };
 
 export default function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        {/* <Circle initial="start" animate="end" /> */}
-        <Circle variants={cicleVariants} />
-        <Circle variants={cicleVariants} />
-        <Circle variants={cicleVariants} />
-        <Circle variants={cicleVariants} />
-      </Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
