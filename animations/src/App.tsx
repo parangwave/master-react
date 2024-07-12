bb3979165974704fc5bcecb2bcf1069bedab79bc
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-13 01:46:58 +0900

    Animations - MotionValues pt.2
    useTransform(motionValue, input, output)으로 한 motionValue의 값을 다른 값으로 map하거나 transform할 수 있음.

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -18,24 +18,10 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-// variants = stage for animations
-// 컴포넌트가 가질 수 있는 미리 정의된 시각적 state
-// 1. help you clean code (mv setting to seperated js obj)
-// 2. combine many animations
-const boxVariants: Variants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
 
 export default function App() {
   // MotionValue !== ReactJS.state == no re-render, not trigger ReactJs Rendering Cycle
